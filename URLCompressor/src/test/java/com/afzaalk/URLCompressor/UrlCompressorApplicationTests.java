295fc372fd2f9c38847f54cd844d18a4c542fd95
afzaalkhan <afzaalkhan@localhost>
2025-07-06 09:37:02 +0300

    Initial Commit

---
+++ URLCompressor/src/test/java/com/afzaalk/URLCompressor/UrlCompressorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afzaalk.URLCompressor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlCompressorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
